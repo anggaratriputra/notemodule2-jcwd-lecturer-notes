f1bb5effde43c0c174acd0a679833ac92a9000e7
ridhozhr10 <azharridho42@gmail.com>
2023-09-05 11:37:23 +0700

    session 13: deploy netlify

--- session12-socia-app/src/api.js
+++ session12-socia-app/src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let baseURL = "http://localhost:3001";
+if (process.env.NODE_ENV === "production") {
+  baseURL = "https://my-json-server.typicode.com/ridhozhr10/notemodule2-jcwd";
+}
+
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL,
 });
 
 export default api;
